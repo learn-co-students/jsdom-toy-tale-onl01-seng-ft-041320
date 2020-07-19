811a261d3a2d135223603c22fa7faf754e1e9d78
mattenbar <matthw.enbar@gmail.com>
2020-07-19 13:59:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 let addToy = false;
 const main = document.querySelector('#toy-collection')
 
+// getting toys from the JSON
 function fetchToys(){
   return fetch("http://localhost:3000/toys")
   .then(res => res.json())
@@ -15,18 +16,59 @@ function renderToys(toys){
   // use of for...of bc the return was an array
   // if the return is and object use for...in
   for (const toy of toys){
+
+    // declaring all the elements inside loop so makes new
+    // element for each toy
     let divCard = document.createElement('div')
     let h2 = document.createElement('h2')
     let img = document.createElement('img')
     let p = document.createElement('p')
+    let btn = document.createElement('button')
+
+    // setting the attributes for each element declared above
     h2.innerText = toy.name
     img.setAttribute('src', toy.image)
     img.setAttribute('class', 'toy-avatar')
     p.innerText = `${toy.likes} likes`
     divCard.setAttribute('class', 'card')
-    divCard.append(h2, img, p)
+    // setting atributs for like button
+    btn.setAttribute('class', 'like-btn')
+    btn.setAttribute('id', toy.id)
+    btn.innerText = "like"
+
+    // event listener for the like button
+    btn.addEventListener('click', (e) => {
+      // calls the like function
+      likes(e)
+    })
+
+    // adds everything to the single character's div created
+    divCard.append(h2, img, p, btn)
+    // adds the character to the page
     main.append(divCard)
-}}
+  }
+}
+
+function likes(e) {
+  e.preventDefault()
+  let more = parseInt(e.target.previousElementSibling.innerText) + 1
+
+  fetch(`http://localhost:3000/toys/${e.target.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+
+      },
+      body: JSON.stringify({
+        "likes": more
+      })
+    })
+    .then(res => res.json())
+    .then((like_obj => {
+      e.target.previousElementSibling.innerText = `${more} likes`;
+    }))
+}
 
 function postToy(toy_data){
   return fetch('http://localhost:3000/toys',{
@@ -43,10 +85,8 @@ function postToy(toy_data){
     } )
   })
   .then(res => res.json())
-  .then((obj_toy) => {
-    debugger
-    let new_toy = renderToys(obj_toy)
-    main.append(new_toy)
+  .then(() => {
+    fetchToys()
   })
 }
 
