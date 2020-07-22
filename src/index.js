cd931cec254802aa4d125f08b82ec6c1323032bf
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-07-22 19:40:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
-let addToy = false;
+// Fetch Andy's Toys
 
+let toysContainer = document.getElementById('toy-collection')
+let toysPath = 'http://localhost:3000/toys'
+
+fetch(toysPath)
+    .then(function(responseObject){
+      return responseObject.json()
+    })
+    // .then(console.log)
+    .then(function(resultsArray){
+      // Add Toy Info to the Card
+      resultsArray.forEach(makeToyDiv)
+      })
+
+function makeToyDiv(toy){
+    toysContainer.innerHTML += `
+    <div id='toy_${toy.id}' class='card'>
+    <h2>${toy.name}</h2>
+    <img src='${toy.image}' class='toy-avatar'></img>
+    <p>${toy.likes} Likes</p>
+    <button class='like-btn'>Like <3</button>
+    </div>
+    `
+  }
+
+// Add a New Toy
+
+// Select
+// pre-done already below
+
+// Listen
+// pre-done already below
+
+let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -8,8 +41,40 @@ document.addEventListener("DOMContentLoaded", () => {
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
+      toyFormContainer.addEventListener('submit', function (event) {
+        event.preventDefault()
+        postToy(event.target)
+      })
     } else {
       toyFormContainer.style.display = "none";
     }
   });
 });
+
+// Do
+
+function postToy(toy) {
+  return fetch('http://localhost:3000/toys', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "name": toy.name.value,
+      "image": toy.image.value,
+      "likes": 0
+    })
+  })
+
+  .then(function (responseObject) {
+    return responseObject.json()
+  })
+  .then(function (resultsObject) {
+    let newToy = makeToyDiv(resultsObject)
+    toysContainer.append(newToy)
+  })
+}
+
+// Increase Toy's Likes
+
