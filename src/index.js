797cdd954aa42823e856dbeccef377498cd7f4a3
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-04 16:12:42 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 let addToy = false;
 
+let toyUrl = "http://localhost:3000/toys";
+
+function getToys(){
+  return fetch(toyUrl)
+  .then(res => res.json())
+  .then(results =>{
+    results.message.forEach(toy => createDiv(toy));
+  });
+};
+
+function createDiv(toy){
+  console.log
+
+};
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -13,3 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+//When the page loads, make a 'GET' request to fetch all the toy objects. []
+//With the response data, make a <div class="card"> for each toy []
+//add it to the toy-collection div []
